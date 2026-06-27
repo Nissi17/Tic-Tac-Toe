6320b22783b5d6782fed14c72ebefae788345733
Nissi17 <nissi200527@gmail.com>
2026-06-27 12:15:43 +0530

    Refactor Tic-Tac-Toe game with function implementations

--- Tictac.py
+++ Tictac.py
@@ -45,8 +45,11 @@ def print_board(board):
 
 def check_winner(board, player):
     """Check if the given player has won."""
+
     # Check rows
-    
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
 
     # Check columns
     for col in range(3):
@@ -54,7 +57,11 @@ def check_winner(board, player):
             return True
 
     # Check diagonals
-    
+    if all(board[i][i] == player for i in range(3)):
+        return True
+
+    if all(board[i][2 - i] == player for i in range(3)):
+        return True
 
     return False
 
